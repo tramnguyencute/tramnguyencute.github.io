0df97d009372c79f4a11564a0c80693be413d6c8
duybang140494 <duybang140494@gmail.com>
2020-11-11 14:11:45 +0700

    Changes to be committed: 	modified:   zzz_push_file_git.sh         Chinh sua hien thi log khi commit file len git.

--- zzz_push_file_git.sh
+++ zzz_push_file_git.sh
@@ -2,6 +2,6 @@
 git add .
 echo "Nhap noi dung commit tren Editor > Save > Close"
 git commit ##
-git log -p master -1
+# git log -p master -1
 git push
 read -n 1 -r -s -p $'Press enter to exit...\n'
