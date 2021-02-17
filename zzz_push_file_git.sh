acdb4aa695e85c26bf7cb7f5f4eb0e3878745d46
duybang140494 <duybang140494@gmail.com>
2021-02-17 14:50:55 +0700

    Xóa file vì không còn cần thiết.

--- zzz_push_file_git.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-git add .
-echo "Nhap noi dung commit tren Editor > Save > Close(Ctrl+F4 for VScode)"
-git commit ##
-# git log -p master -1
-git push
-read -n 1 -r -s -p $'Press enter to exit...\n'
